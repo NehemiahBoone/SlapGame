690ab72eaa899fd8e7a93b2c0f77081f99370211
Nehemiah Boone <nehemiah.boone@gmail.com>
2020-08-20 14:59:24 -0600

    progress

--- game.js
+++ game.js
@@ -1,9 +1,28 @@
 let health = 100
-let healthStatus = document.getElementById("health")
+let hits = 0
 
+let healthStatus = document.getElementById("health")
+let hitStatus = document.getElementById("hits")
+let targetName = document.getElementById("name")
+targetName.innerHTML = `
+<h1>Scarecrow</h1>
+`
 
 function slap() {
   health--
+  hits++
+  update()
+}
+
+function punch() {
+  health -= 5
+  hits++
+  update()
+}
+
+function kick() {
+  health -= 10
+  hits++
   update()
 }
 
@@ -11,8 +30,16 @@ function update() {
   healthStatus.innerHTML = `
   ${health}
   `
+
+  hitStatus.innerHTML = `
+  ${hits}
+  `
 }
 
+function restartGame() {
+  if (health == 0) {
 
+  }
+}
 
 update()
